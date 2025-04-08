4e5830e46d7474049d650b1399e5b28175162808
Mateo_Jurado <hernan.jurado@udla.edu.ec>
2025-04-08 13:37:27 -0500

    Creacion del proyecto, prueba del servicio

--- Proyuecto_HJ/urls.py
+++ Proyuecto_HJ/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Inventario.views import Hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('', Inventario.views.hello),
 ]
