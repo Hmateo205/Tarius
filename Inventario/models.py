2083456dd33da9e39965eb4cd9ff260d77c78603
Mateo_Jurado <hernan.jurado@udla.edu.ec>
2025-04-09 05:03:58 -0500

    Admin

--- Inventario/models.py
+++ Inventario/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Inventario(models.Model):
-    titulo = models.CharField
-    Descripcion =models.TextField
-    Cantidad = models.IntegerField
+class Stock(models.Model):
+    Titulo = models.CharField(max_length=100)
+    Descripcion =models.TextField(blank=True)
+    Cantidad = models.IntegerField(max_length=100)
     Fecha_Creacion =models.DateField (auto_now_add=True)
     Comprar =   models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__ (self):
+        return self.Titulo + '/ Creado por:' + self.user.username
