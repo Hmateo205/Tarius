99b4880104cd3b50cb661e59533bc653199834e5
Mateo_Jurado <hernan.jurado@udla.edu.ec>
2025-04-09 04:41:28 -0500

    Migraet, Super user

--- Inventario/models.py
+++ Inventario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Inventario(models.Model):
+    titulo = models.CharField
+    Descripcion =models.TextField
+    Cantidad = models.IntegerField
+    Fecha_Creacion =models.DateField (auto_now_add=True)
+    Comprar =   models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
