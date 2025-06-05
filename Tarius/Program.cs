04159bfc77eedc13b02063dac6a48f45dfa9b2b8
Mateo_Jurado <hernan.jurado@udla.edu.ec>
2025-06-05 10:44:22 -0500

    Ultima actualizacion

--- Tarius/Program.cs
+++ Tarius/Program.cs
@@ -6,12 +6,6 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Agrega esta parte para que cargue appsettings.Production.json en Azure
-builder.Configuration
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables();
 
 // Configuración de la base de datos
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
